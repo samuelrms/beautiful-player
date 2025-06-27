74cc8f3a292dcd7cc6c3c308efe9682fbc5ecaff
Samuel Ramos <samuelaoliveiraramos@gmail.com>
2025-06-26 22:11:45 -0300

    Add hideButtons prop to BeautifulAudio component for button visibility control

--- wrappers/vue.ts
+++ wrappers/vue.ts
@@ -6,18 +6,37 @@ export const BeautifulAudio = defineComponent({
     name: 'BeautifulAudio',
     props: {
         src: { type: String, required: true },
-        speeds: { type: String, default: '1,1.5,2' },
-        autoplay: Boolean,
-        primaryColor: String
+        speeds: { type: String, default: undefined },
+        autoplay: { type: Boolean, default: false },
+        primaryColor: { type: String, default: undefined },
+        hideButtons: {
+            type: Object as () => { speed?: boolean; volume?: boolean; download?: boolean },
+            default: () => ({}),
+        },
     },
-    setup(props, { attrs }) {
-        return () =>
-            h('beautiful-audio', {
-                ...attrs,
-                src: props.src,
-                speeds: props.speeds,
-                autoplay: props.autoplay,
-                'primary-color': props.primaryColor
-            });
+    mounted() {
+        const { primaryColor, hideButtons } = this.$props as any;
+        if (primaryColor) this.$el.setAttribute('primary-color', primaryColor);
+
+        if (hideButtons) {
+            if (hideButtons.speed) this.$el.setAttribute('hide-speed', '');
+            if (hideButtons.volume) this.$el.setAttribute('hide-volume', '');
+            if (hideButtons.download) this.$el.setAttribute('hide-download', '');
+            const list = [
+                hideButtons.speed ? 'speed' : null,
+                hideButtons.volume ? 'volume' : null,
+                hideButtons.download ? 'download' : null,
+            ].filter(Boolean).join(',');
+            if (list) this.$el.setAttribute('hide-buttons', list);
+        }
+    },
+    render() {
+        return h('beautiful-audio', {
+            ...this.$attrs,
+            src: this.src,
+            speeds: this.speeds,
+            autoplay: this.autoplay,
+            'primary-color': this.primaryColor,
+        });
     }
 }); 
